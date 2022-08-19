22fdb94a4fa214513872f2a73bf2c2ba4c8ea8d1
sheila muzy <machadomarcos2019@gmail.com>
2022-08-19 01:10:15 -0300

    .gitignore funcionando ;)

--- src/database/Schemas/Levelroles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const LevelRoleSchema = new mongoose.Schema({
-  guildID: { type: String },
-  role: { type: String, default: null },
-  level: {type: Number, default: null },
-});
-
-module.exports = mongoose.model('LevelsRoles', LevelRoleSchema);
--- src/database/Schemas/Models.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    guilds: require('./Guild'),
-    users: require('./User.js'),
-    fundos: require('./Fundo.js'),
-    levelsroles: require('./Levelroles'),
-    shop: require('./Shop'),
-    shopitems: require('./ShopItems'),
-    codes: require('./Codes')
-}
